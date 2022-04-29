7902095991c7887ddf4dbced2a7fff6df781c960
Nina NJul <nina.dev.web@gmail.com>
2022-04-29 07:05:44 +0300

    init: start lesson-2-home-task

---
+++ myproj/src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { Footer } from '../Footer/Footer';
+
+const App = () => (
+  <div className='App'>
+    <Footer copyright='NJul © 2022' />
+  </div>
+);
+
+export default App;
